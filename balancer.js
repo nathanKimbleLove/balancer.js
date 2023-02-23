1d09528a0cf16546295d6a8513bdb5de1cfc5508
Nathan Love <nlovephd@gmail.com>
2023-02-23 14:39:46 -0600

    enable loaderio testing

--- balancer.js
+++ balancer.js
@@ -3,7 +3,7 @@ const app = express()
 
 const axios = require('axios');
 
-const balancer = (port = 8080, ...ips) => {
+const balancer = (port = 8080, ips, loaderio) => {
 
   //choose ips round-robin style
   let counter = 0;
@@ -18,19 +18,21 @@ const balancer = (port = 8080, ...ips) => {
   app.use(express.json());
   app.get('/test-balancer-test', (req, res) => res.send('success'));
 
+  app.get(`/${loaderio}`, (req, res) => res.send(loaderio));
+
   app.get('/*', (req, res) => {
-    console.log(req.headers)
-    axios.get(picker() + req.url, {headers: req.headers})
+    axios.get(picker() + req.url)
     .then(resp => {
       res.send(resp.data)
     })
     .catch(err => {
+      console.log(err);
       res.status(400).send(err)
     })
   })
 
   app.post('/*', (req, res) => {
-    axios.post(picker() + req.url, req.body, {headers: req.headers})
+    axios.post(picker() + req.url, req.body)
     .then(resp => {
       res.send(resp.data)
     })
@@ -45,6 +47,6 @@ const balancer = (port = 8080, ...ips) => {
 
 }
 
-balancer(8010, 'http://localhost:8001', 'http://localhost:8002', 'http://localhost:8003')
+balancer(8010, ['http://18.219.227.167:3000', 'http://18.222.51.19:3000']);
 
 module.exports = balancer
